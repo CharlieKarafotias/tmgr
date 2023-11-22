1d7850401fae9bcbbe28a10d937e2689811508e7
Charles Karafotias <cnkara2023@gmail.com>
2023-11-21 19:46:31 -0500

    adding initial commands

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,10 @@ struct Cli {
 
 #[derive(Debug, Subcommand)]
 enum Command {
-    Database,
+    Database {
+        #[command(subcommand)]
+        command: Database,
+    },
     Status,
     Todo {
         #[command(subcommand)]
@@ -18,14 +21,39 @@ enum Command {
     },
 }
 
+// Declarations for the Todo command
 #[derive(Debug, Subcommand)]
 enum Todo {
+    /// Add a new task
     Add {
         /// A short description of the task
         name: String,
         /// The priority of the task
         #[clap(default_value_t = TaskPriority::High)]
         priority: TaskPriority,
+        /// An optional long description of the task
+        description: Option<String>,
+    },
+    /// Mark a task as complete
+    Complete {
+        /// The id of the task
+        id: i32,
+    },
+    /// Delete a task
+    Delete {
+        /// The id of the task
+        id: i32,
+    },
+    /// List all tasks
+    List,
+    /// Update an existing task
+    Update {
+        /// The id of the task
+        id: i32,
+        /// A short description of the task
+        name: Option<String>,
+        /// The priority of the task
+        priority: Option<TaskPriority>,
         /// An optional long description of the task
         description: Option<String>,
     },
@@ -47,6 +75,27 @@ impl std::fmt::Display for TaskPriority {
     }
 }
 
+// Declarations for the Database command
+#[derive(Debug, Subcommand)]
+enum Database {
+    /// Add a new database
+    Add {
+        /// The database name
+        name: String,
+    },
+    /// Delete an existing database
+    Delete {
+        /// The database name
+        name: String,
+    },
+    /// List all databases
+    List,
+    /// Set the working database
+    Set {
+        /// The database name
+        name: String,
+    },
+}
 fn main() {
     let cli = Cli::parse();
     println!("{:?}", cli);
