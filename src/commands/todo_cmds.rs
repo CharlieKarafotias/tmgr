a315db583d89392c232d55192a05319518d7e3ca
Charles Karafotias <cnkara2023@gmail.com>
2024-03-01 21:14:45 -0500

    common format for todo commands following db_cmds pattern

--- src/commands/todo_cmds.rs
+++ src/commands/todo_cmds.rs
@@ -4,20 +4,55 @@ use super::db_cmds::db::DB;
 
 pub fn add(state: &mut State, name: String, priority: TaskPriority, description: Option<String>) {
     let db = connect_to_db(state);
-    let res = db.add_task(name, priority.to_string(), description);
-    println!("{:?}", res);
+    match db {
+        Ok(db) => {
+            let res = db.add_task(name, priority.to_string(), description);
+            match res {
+                Ok(_) => println!("Successfully added task"),
+                Err(e) => println!("ERROR: {}", e),
+            }
+        }
+        Err(e) => println!("ERROR: {}", e),
+    }
 }
 pub fn complete(state: &mut State, id: i64) {
     let db = connect_to_db(state);
-    let _ = db.complete_todo(id);
+    match db {
+        Ok(db) => {
+            let res = db.complete_todo(id);
+            match res {
+                Ok(_) => (), // TODO: complete_todo should return number of rows updated. The commands should handle printing
+                Err(e) => println!("ERROR: {}", e),
+            }
+        }
+        Err(e) => println!("ERROR: {}", e),
+    }
 }
 pub fn delete(state: &mut State, id: i64) {
     let db = connect_to_db(state);
-    let _ = db.delete_todo(id);
+    match db {
+        Ok(db) => {
+            let res = db.delete_todo(id);
+            match res {
+                Ok(_) => (), // TODO: delete_todo should return number of rows deleted. The commands should handle printing
+                Err(e) => println!("ERROR: {}", e),
+            }
+        }
+        Err(e) => println!("ERROR: {}", e),
+    }
 }
 pub fn list(state: &mut State) {
     let db = connect_to_db(state);
-    db.list_tasks().unwrap();
+    match db {
+        Ok(db) => {
+            let res = db.list_tasks();
+            match res {
+                Ok(_) => (), // TODO: list_tasks should return vector. The commands should handle printing
+                Err(e) => println!("ERROR: {}", e),
+            }
+        }
+        Err(e) => println!("ERROR: {}", e),
+    }
 }
 pub fn update(
     state: &mut State,
@@ -27,13 +62,20 @@ pub fn update(
     description: Option<String>,
 ) {
     let db = connect_to_db(state);
-    let mut priority_str: Option<String> = None;
-    if let Some(priority) = priority {
-        priority_str = Some(priority.to_string());
+    match db {
+        Ok(db) => {
+            let priority_str: Option<String> = priority.map(|p| p.to_string());
+
+            let res = db.update_todo(id, name, priority_str, description);
+            match res {
+                Ok(_) => println!("Successfully updated task"),
+                Err(e) => println!("ERROR: {}", e),
+            }
+        }
+        Err(e) => println!("ERROR: {}", e),
     }
-    let _ = db.update_todo(id, name, priority_str, description);
 }
 
-fn connect_to_db(state: &mut State) -> DB {
-    DB::new(state).unwrap()
+fn connect_to_db(state: &mut State) -> Result<DB, Box<dyn std::error::Error>> {
+    DB::new(state)
 }
