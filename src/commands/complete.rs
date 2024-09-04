ec7c5ebcaeb87dfc79fc17ef9b70e97159491028
Charles Karafotias <cnkara2023@gmail.com>
2024-09-04 11:17:02 -0400

    updating complete command; adding better tests for complete command

--- src/commands/complete.rs
+++ src/commands/complete.rs
@@ -3,12 +3,13 @@ use crate::commands::db::DB;
 pub(crate) async fn run(db: &DB, name: String) -> Result<String, Box<dyn std::error::Error>> {
     // ~ compares 2 values for equality using fuzzy match
     // https://surrealdb.com/docs/surrealdb/surrealql/operators#match
-    // TODO: Should add a check; if 2 tasks are close, give user the option to choose
     let query = format!(
-        "UPDATE task SET completed_at = time::now() WHERE name ~ {}",
+        "UPDATE task SET completed_at = time::now() WHERE completed_at IS None AND name ~ '{}'",
         name
     );
+    // TODO: Should add a check; if 2 tasks are close, give user the option to choose
     db.client.query(&query).await?;
+
     // TODO: if no task was updated, it would be nice to have different message
     Ok("Successfully updated task to completed".to_string())
 }
--- src/commands/tests/complete_test.rs
+++ src/commands/tests/complete_test.rs
@@ -1,5 +1,9 @@
-#[allow(unused_imports)]
+#![allow(unused_imports)]
+
+use crate::cli::model::TaskPriority;
+use crate::commands::model::Task;
 use crate::commands::{complete, db};
+
 #[tokio::test]
 async fn given_no_existing_tasks_when_completing_a_task_then_no_task_should_be_completed() {
     let db = db::DB::new_test().await;
@@ -11,4 +15,85 @@ async fn given_no_existing_tasks_when_completing_a_task_then_no_task_should_be_c
         res_str,
         "Successfully updated task to completed".to_string()
     );
+}
+
+#[tokio::test]
+async fn given_existing_tasks_when_completing_a_task_then_the_task_should_be_completed() {
+    let db = db::DB::new_test().await;
+    let _: Vec<Task> = db
+        .client
+        .insert("task")
+        .content(Task {
+            name: "in progress task".to_string(),
+            priority: TaskPriority::High.to_string(),
+            description: None,
+            created_at: Default::default(),
+            completed_at: None,
+        })
+        .await
+        .unwrap();
+    let res = complete::run(&db, "in progress task".to_string()).await;
+    assert!(res.is_ok());
+    let res_str = res.unwrap();
+    assert_eq!(
+        res_str,
+        "Successfully updated task to completed".to_string()
+    );
+
+    let res: Vec<Task> = db.client.select("task").await.unwrap();
+    assert_eq!(res.len(), 1);
+    assert_ne!(res[0].completed_at, None);
+}
+
+// TODO: this test will not work now because there is no disambiguation between two tasks with the similar names
+#[tokio::test]
+#[ignore]
+async fn given_two_tasks_with_similar_names_when_completing_task_by_name_then_the_task_with_the_closest_name_should_be_completed(
+) {
+    let db = db::DB::new_test().await;
+    let _: Vec<Task> = db
+        .client
+        .insert("task")
+        .content(Task {
+            name: "one task".to_string(),
+            priority: TaskPriority::High.to_string(),
+            description: None,
+            created_at: Default::default(),
+            completed_at: None,
+        })
+        .await
+        .unwrap();
+    let _: Vec<Task> = db
+        .client
+        .insert("task")
+        .content(Task {
+            name: "one more task".to_string(),
+            priority: TaskPriority::Low.to_string(),
+            description: None,
+            created_at: Default::default(),
+            completed_at: None,
+        })
+        .await
+        .unwrap();
+
+    let res = complete::run(&db, "one t".to_string()).await;
+    assert!(res.is_ok());
+    let res_str = res.unwrap();
+    assert_eq!(
+        res_str,
+        "Successfully updated task to completed".to_string()
+    );
+
+    let res: Vec<Task> = db.client.select("task").await.unwrap();
+    assert_eq!(res.len(), 2);
+    let first = &res[0];
+    let second = &res[1];
+    let mut completed_count = 0;
+    if first.completed_at.is_some() {
+        completed_count += 1;
+    }
+    if second.completed_at.is_some() {
+        completed_count += 1;
+    }
+    assert_eq!(completed_count, 1);
 }
