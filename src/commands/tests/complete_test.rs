7b979300e11993403bba5cd72766afb95b89656a
Charles Karafotias <cnkara2023@gmail.com>
2024-09-07 08:57:31 -0400

    added test to complete command suite to ensure data stays same

--- src/commands/tests/complete_test.rs
+++ src/commands/tests/complete_test.rs
@@ -91,3 +91,40 @@ async fn given_two_tasks_with_similar_names_when_completing_task_by_name_then_th
     }
     assert_eq!(completed_count, 1);
 }
+
+#[tokio::test]
+async fn given_existing_tasks_when_completing_a_task_then_the_other_parts_of_the_task_should_stay_the_same(
+) {
+    let db = db::DB::new_test().await;
+    let _: Option<Task> = db
+        .client
+        .insert(("task", "task to complete"))
+        .content(Task {
+            name: "task to complete".to_string(),
+            priority: TaskPriority::Medium.to_string(),
+            description: Some("This is a description of the task".to_string()),
+            created_at: Default::default(),
+            completed_at: None,
+        })
+        .await
+        .unwrap();
+
+    let res = complete::run(&db, "task to complete".to_string()).await;
+    assert!(res.is_ok());
+    let res_str = res.unwrap();
+    assert_eq!(
+        res_str,
+        "Successfully updated task 'task to complete' to completed".to_string()
+    );
+
+    let res: Vec<Task> = db.client.select("task").await.unwrap();
+    assert_eq!(res.len(), 1);
+    let first = &res[0];
+    assert!(first.completed_at.is_some());
+    assert_eq!(first.name, "task to complete".to_string());
+    assert_eq!(first.priority, TaskPriority::Medium.to_string());
+    assert_eq!(
+        first.description,
+        Some("This is a description of the task".to_string())
+    );
+}
